851372f95d6315c8ab7e4cd8a02487de8de5ea4f
Crispin <cooperch@cf.ac.uk>
2019-05-31 14:30:30 +0100

    Revert "Change docs build path to docs and build process to put html in docs; check in doc build"
    This reverts commit 3b409817ec8ce90a92d72a7b649a91857a7924cd.

--- installerbits/rename_version.py
+++ installerbits/rename_version.py
@@ -18,3 +18,19 @@ if (os.path.exists(outfilename)):
     
 os.rename(installfile,outfilename)
 
+outdocname = "%s/sDNA_open_manual_v%s"%(outputdir,filename_friendly_version)
+
+shutil.rmtree(outdocname,ignore_errors=True)
+shutil.copytree(docdir,outputdir+os.sep+outdocname)
+
+redirfile = open(outputdir+"/latest.html","w")
+redirfile.write("""
+<html><head>
+<meta http-equiv="refresh" content="0; url=%s" />
+</head>
+<body>
+If you are not redirected please click <a href="%s">here</a>.
+</body>
+</html>
+"""%(outdocname,outdocname))
+redirfile.close()
