333f93e6bb0404a634082d3117dac18ac2a4c0e4
Crispin <cooperch@cf.ac.uk>
2019-05-31 15:03:40 +0100

    reinstate sphinx extensions

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -30,7 +30,9 @@ import shlex
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.mathjax'
+    'sphinx.ext.mathjax',
+    'sphinxcontrib.fulltoc',
+    'sphinx_numfig'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
